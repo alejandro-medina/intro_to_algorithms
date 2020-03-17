18ad915e453dd25e18872aab488197fb12dcc51c
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2020-03-16 20:51:52 -0600

    Modified the program calculator.c

--- project/calculator.c
+++ project/calculator.c
@@ -1,53 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float modulo(float a, float b){
+float module(float a, float b){ return (int)a % (int)b; }
+float add(float a, float b) { return a+b; }
+float subtract(float a, float b){ return a-b; }
+float multiply(float a, float b){ return a*b; }
+float divide(float a, float b){ return a/b; }
 
-	return (int)a % (int)b;
+void showExample(){
+
+	printf("Example of use: \t$ ./a.out 4 + 3\n");
+	printf("Outpul will be: \t>> 7\n");
+	printf("\nList of operators: \n\n");
+	printf("Multiplication -----> 'x'\n");
+	printf("Division -----------> '/'\n");
+	printf("Addition -----------> '+'\n");
+	printf("Subtraction --------> '-'\n");
+	printf("Module -------------> '%%'\n\n");
+
 }
 
-float sumar(float a, float b) {
-	return a+b;
-}
-
-float restar(float a, float b){
-	return a-b;
-}
-
-float multiplicar(float a, float b){
-	return a*b;
-}
-
-float dividir(float a, float b){
-	return a/b;
-}
+int main(int argc, char** argv){
 
 
-void main(int argc, char** argv){
+	int arguments = argc-1;
 
-	char symbol = *argv[1];
-	float a = atof(argv[2]);
-	float b = atof(argv[3]);
+	if(arguments == 0) {
+		
+		printf("\nInsert arguments to the program so it can do the calculations.\n");
+		showExample();
+		return 0;
 
-	switch(symbol){
-		case '+':
-			printf("%.2f\n", sumar(a, b));
-		break;
-		case '-':
-			printf("%.2f\n", restar(a, b));
-		break;
-		case '/':
-			printf("%.2f\n", dividir(a, b));
-		break;
-		case '%':
-			printf("%d MOD %d = %.2f\n", (int)a, (int)b, modulo(a, b));
-		break;
-		case 'x':
-			printf("%.2f\n", multiplicar(a, b));
-		break;
-		default:
-			printf("Operador no conocido\n");
-		break;
+	} else if(arguments < 3 || arguments > 3){
+
+		printf("\nInsert three argument command line as in the example:\n\n");
+		printf("Arguments should be the form: \t [number] [operator] [number]\n");
+		showExample();
+		return 0;
+	} else {
+		char symbol = (char)*argv[2];
+		float a = atof(argv[1]);
+		float b = atof(argv[3]);
+
+		switch(symbol){
+			case '+':
+				printf("%.2f\n", add(a, b));
+			break;
+			case '-':
+				printf("%.2f\n", subtract(a, b));
+			break;
+			case '/':
+				printf("%.2f\n", divide(a, b));
+			break;
+			case '%':
+				printf("%d MOD %d = %.2f\n", (int)a, (int)b, module(a, b));
+			break;
+			case 'x':
+				printf("%.2f\n", multiply(a, b));
+			break;
+			default:
+				printf("Unknown operator %c\n", symbol);
+			break;
+		}
+
 	}
+
+	return 0;
 
 }
